7da0d3eb185ee2dd69a4fb9e728138d2a5ca63f4
Bradley Barnes <48269593+capitalbr@users.noreply.github.com>
2025-04-10 15:05:27 -0400

    Testing git email set up

--- test/specs/test.typos.js
+++ test/specs/test.typos.js
@@ -1,5 +1,6 @@
 import landingPage from "../pageobjects/landing.page";
 
+// testing git email set up
 describe('Typos tests', () => {
     it('Displays the text with no typos', async() => {
         await landingPage.open();
